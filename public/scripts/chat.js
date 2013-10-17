0c8c2cce52e2adc4dd741a4018b6568edd6da67e
Jonathan Lebron <jlebron2@gmail.com>
2013-10-17 01:25:57 -0400

    moved stuff around

---
+++ public/scripts/chat.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  var messages = [],
+      socket = io.connect('http://localhost:8080'),
+      chat = $('#chat'),
+      username = $('#username'),
+      text = $('#text');
+
+  socket.on('message', function (data) {
+    if(data.message) {
+      messages.push(data);
+      var html = '';
+      for(var i=0; i<messages.length; i++) {
+        html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+        html += messages[i].message + '<br />';
+      }
+      chat.html(html);
+    } else
+      console.log("There is a problem:", data);
+  });
+
+  $('#text').on('keydown', function(e){
+    var key = e.which || e.keyCode;
+    if(key == 13)
+      handleMessage();
+  });
+
+  $('#sendButton').on('click', function(){
+    handleMessage();
+  });
+
+  function handleMessage() {
+    if( username.val() == "" )
+      alert("Please type your name!");
+    else {
+      socket.emit('send', { message: text.val(), username: username.val() });
+      text.val('');
+    }
+  }
+});
